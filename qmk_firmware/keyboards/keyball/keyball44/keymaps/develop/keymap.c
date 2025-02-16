8e1d8b303cdb03e8dbb3a4621d61d178f2b5cc35
aki2ba <yorunoakarito@gmail.com>
2025-02-16 21:54:33 +0900

    henk,mhen入れ替え

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/develop/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/develop/keymap.c
@@ -41,31 +41,31 @@ combo_t key_combos[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
   [0] = LAYOUT_universal(
-    KC_INT4    , KC_Q     , KC_W         , KC_E           , KC_R           , KC_T     ,                                        KC_Y     , KC_U        , KC_I           , KC_O            , KC_P     , KC_BSPC   ,
+    KC_TAB     , KC_Q     , KC_W         , KC_E           , KC_R           , KC_T     ,                                        KC_Y     , KC_U        , KC_I           , KC_O            , KC_P     , KC_BSPC   ,
     KC_INT4    , KC_A     , KC_S         , CTL_T(KC_D)    , LT(2,KC_F)     , KC_G     ,                                        KC_H     , LT(2,KC_J)  , CTL_T(KC_K)    , ALT_T(KC_L)     , TO(2)    , TO(3)     ,
     KC_INT5    , KC_Z     , SFT_T(KC_X)  , LT(3,KC_C)     , LT(1,KC_V)     , KC_B     ,                                        KC_N     , LT(1,KC_M)  , LT(3,KC_COMM)  , 	SFT_T(KC_DOT) , KC_SLSH  , KC_DEL    ,
-                TO(2)    , _______      , _______          , KC_SPC       , KC_ENT   ,                       KC_LWIN,   KC_TAB, 	KC_ESC,     KC_MINS  , S(KC_INT1)
+                TO(2)    , _______      , _______          , KC_SPC       , KC_ENT   ,                       KC_ESC,   KC_LWIN, 	KC_CAPS,     KC_MINS  , S(KC_INT1)
   ),
 
   [1] = LAYOUT_universal(
-    TO0_HENK   ,  S(KC_EQL)  , KC_1     , KC_2    , KC_3   , S(KC_4)   ,                                               S(KC_LBRC)   , S(KC_1)    , S(KC_2)    , S(KC_3)     , S(KC_SCLN)  , S(KC_QUOT)  ,
-    TO0_HENK     ,  KC_0       , KC_4     , KC_5    , KC_6   , S(KC_5)   ,                                               KC_LBRC      , S(KC_7)    , S(KC_8)    , S(KC_9)     ,  KC_SCLN    , KC_QUOT     ,
-    TO0_MHEN   ,  KC_EQL     , KC_7     , KC_8    , KC_9   , S(KC_6)   ,                                               KC_PGDN      , S(KC_MINS) , KC_MINS    , S(KC_INT1)  , 	KC_NUBS    , KC_DEL      ,
-                  _______  , _______ , _______  ,    KC_SPC,        KC_ENT,                            KC_LWIN    , KC_TAB      ,	KC_ESC       , KC_RBRC    , KC_BSLS
+    KC_TAB     ,  S(KC_EQL)  , KC_1     , KC_2    , KC_3   , S(KC_4)   ,                                               S(KC_LBRC)   , S(KC_1)    , S(KC_2)    , S(KC_3)     , S(KC_SCLN)  , S(KC_QUOT)  ,
+    TO0_MHEN   ,  KC_0       , KC_4     , KC_5    , KC_6   , S(KC_5)   ,                                               KC_LBRC      , S(KC_7)    , S(KC_8)    , S(KC_9)     ,  KC_SCLN    , KC_QUOT     ,
+    TO0_HENK   ,  KC_EQL     , KC_7     , KC_8    , KC_9   , S(KC_6)   ,                                               KC_PGDN      , S(KC_MINS) , KC_MINS    , S(KC_INT1)  , 	KC_NUBS    , KC_DEL      ,
+                  _______  , _______ , _______  ,    KC_SPC,        KC_ENT,                            KC_ESC    , KC_LWIN      ,	KC_CAPS       , KC_RBRC    , KC_BSLS
   ),
 
   [2] = LAYOUT_universal(
-    TO0_HENK  , C(KC_Q)    , KC_W      , C(KC_E)     , C(KC_R)     , C(KC_T)     ,                                        C(KC_Y)     , C(KC_PGUP)     , MS_BTN3        , C(KC_PGDN)      , C(KC_P)    , KC_BSPC   ,
-    TO0_HENK    , C(KC_A)    , MS_BTN1   , MS_BTN3     , MS_BTN2     , C(KC_G)     ,                                        C(KC_H)     , MS_BTN1        , CTL_T(KC_UP)   , 	MS_BTN2        , _______    , TO(3)     ,
-    TO0_MHEN  , C(KC_Z)    , KC_LSFT   , C(KC_C)     , C(KC_V)     , A(KC_LEFT)  ,                                        A(KC_RGHT)  , LT(1,KC_LEFT)  , LT(3,KC_DOWN)  , SFT_T(KC_RGHT)  , KC_SLSH    , KC_DEL    ,
-                  TO(3)     , _______       , _______        ,         KC_SPC      , KC_ENT,                KC_LWIN   , KC_TAB     , KC_ESC           , KC_HOME        , KC_END
+    KC_TAB    , C(KC_Q)    , KC_W      , C(KC_E)     , C(KC_R)     , C(KC_T)     ,                                        C(KC_Y)     , C(KC_PGUP)     , MS_BTN3        , C(KC_PGDN)      , C(KC_P)    , KC_BSPC   ,
+    TO0_MHEN  , C(KC_A)    , MS_BTN1   , MS_BTN3     , MS_BTN2     , C(KC_G)     ,                                        C(KC_H)     , MS_BTN1        , CTL_T(KC_UP)   , 	MS_BTN2        , _______    , TO(3)     ,
+    TO0_HENK  , C(KC_Z)    , KC_LSFT   , C(KC_C)     , C(KC_V)     , A(KC_LEFT)  ,                                        A(KC_RGHT)  , LT(1,KC_LEFT)  , LT(3,KC_DOWN)  , SFT_T(KC_RGHT)  , KC_SLSH    , KC_DEL    ,
+                  TO(3)     , _______       , _______        ,         KC_SPC      , KC_ENT,                KC_ESC   , KC_LWIN     , KC_CAPS           , KC_HOME        , KC_END
   ),
 
   [3] = LAYOUT_universal(
-    TO0_HENK   , KC_F1     , KC_F2    , KC_F3      , KC_F4     , KC_F5    ,                                        KC_F6     , KC_F7     , KC_F8     , KC_F9     , KC_F10    , KC_PSCR   ,
-    TO0_HENK     , KC_F11    , C(KC_S)  , C(KC_D)    , C(KC_F)   , KC_F12   ,                                        _______   , C(KC_J)   , C(KC_K)   , C(KC_L)   , TO(2)     , _______   ,
-    TO0_MHEN   , _______   , C(KC_X)  , S(KC_TAB)  , KC_TAB    , C(KC_B)  ,                                        C(KC_N)   , KC_LALT   , _______   , KC_LSFT   , _______   , _______   ,
-                TO(2)     , _______  , _______    ,        _______  , KC_ESC  ,                   _______  , SS_DC     , SS_LHOST       , KBC_RST  , QK_BOOT
+    KC_TAB     , KC_F1     , KC_F2    , KC_F3      , KC_F4     , KC_F5    ,                                        KC_F6     , KC_F7     , KC_F8     , KC_F9     , KC_F10    , KC_PSCR   ,
+    TO0_MHEN   , KC_F11    , C(KC_S)  , C(KC_D)    , C(KC_F)   , KC_F12   ,                                        _______   , C(KC_J)   , C(KC_K)   , C(KC_L)   , TO(2)     , _______   ,
+    TO0_HENK   , _______   , C(KC_X)  , S(KC_TAB)  , KC_TAB    , C(KC_B)  ,                                        C(KC_N)   , KC_LALT   , _______   , KC_LSFT   , _______   , _______   ,
+                TO(2)     , _______  , _______    ,        _______  , _______  ,                   SS_LHOST  , SS_DC     , _______       , KBC_RST  , QK_BOOT
   ),
 };
 // clang-format on
