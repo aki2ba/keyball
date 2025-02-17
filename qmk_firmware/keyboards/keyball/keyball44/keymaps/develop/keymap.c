df33a0626a4b600ebcb7d1179c8ec4fd1497a0b2
aki2ba <yorunoakarito@gmail.com>
2025-02-17 22:20:37 +0900

    すみませんでした

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/develop/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/develop/keymap.c
@@ -28,6 +28,31 @@ enum custom_keycodes {
   LT3_COPY
 };
 
+enum {
+  TD_L3_CTL_C = 0
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_L3_CTL_C] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, ctl_c_finished, ctl_c_reset)
+};
+
+void ctl_c_finished(qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+      // タップ時に Ctrl+C を送信
+      register_code(KC_LCTL);
+      register_code(KC_C);
+      unregister_code(KC_C);
+      unregister_code(KC_LCTL);
+  } else {
+      // ホールド時にレイヤー3に移行
+      layer_on(3);
+  }
+}
+
+void ctl_c_reset(qk_tap_dance_state_t *state, void *user_data) {
+  layer_off(3);
+}
+
 enum combos {
   CL_CPLK
 };
@@ -37,6 +62,7 @@ const uint16_t PROGMEM cl_combo[] = {KC_C, KC_L, COMBO_END};
 combo_t key_combos[] = {
   [CL_CPLK] = COMBO(cl_combo, S(KC_CAPS)),
 };
+
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -56,9 +82,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [2] = LAYOUT_universal(
-    KC_TAB    , C(KC_Q)    , KC_W      , C(KC_E)     , C(KC_R)     , C(KC_T)     ,                                        C(KC_Y)     , C(KC_PGUP)     , MS_BTN3        , C(KC_PGDN)      , C(KC_P)    , KC_BSPC   ,
-    TO0_MHEN  , C(KC_A)    , MS_BTN1   , MS_BTN3     , MS_BTN2     , KC_ENT      ,                                        C(KC_H)     , MS_BTN1        , CTL_T(KC_UP)   , 	MS_BTN2        , _______    , TO(3)     ,
-    TO0_HENK  , C(KC_Z)    , KC_LSFT   , LT3_COPY    , C(KC_V)     , A(KC_LEFT)  ,                                        A(KC_RGHT)  , LT(1,KC_LEFT)  , LT(3,KC_DOWN)  , SFT_T(KC_RGHT)  , KC_SLSH    , KC_DEL    ,
+    KC_TAB    , C(KC_Q)    , KC_W      , C(KC_E)        , C(KC_R)     , C(KC_T)     ,                                        C(KC_Y)     , C(KC_PGUP)     , MS_BTN3        , C(KC_PGDN)      , C(KC_P)    , KC_BSPC   ,
+    TO0_MHEN  , C(KC_A)    , MS_BTN1   , MS_BTN3        , MS_BTN2     , KC_ENT      ,                                        C(KC_H)     , MS_BTN1        , CTL_T(KC_UP)   , 	MS_BTN2        , _______    , TO(3)     ,
+    TO0_HENK  , C(KC_Z)    , KC_LSFT   , TD(TD_L3_CTL_C), C(KC_V)     , A(KC_LEFT)  ,                                        A(KC_RGHT)  , LT(1,KC_LEFT)  , LT(3,KC_DOWN)  , SFT_T(KC_RGHT)  , KC_SLSH    , KC_DEL    ,
                   TO(3)     , _______       , _______        ,         KC_SPC      , KC_ENT,                KC_ESC   , KC_LWIN     , S(KC_CAPS)           , KC_HOME        , KC_END
   ),
 
@@ -106,19 +132,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
               layer_move(0);
               tap_code16(KC_INT5);
           }
+          return false;
       case TO0_HENK:
           if (record->event.pressed) {
               layer_move(0);
               tap_code16(KC_INT4);
           }
-      case LT3_COPY:
-          if (record->tap.count && record->event.pressed) {
-              tap_code16(C(KC_C));
-          } else if (record->event.pressed) {
-              layer_on(3);
-          } else {
-              layer_off(3);
-          }
+          return false;
   }
 
   return true;
