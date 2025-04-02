a762f0a7a0513e9b8004aee5e2563bf27dc07dc1
kosaka_izumi <96470555+aki2ba@users.noreply.github.com>
2025-04-02 14:23:48 +0900

    マウス移動を5以上にする

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/develop/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/develop/keymap.c
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-  if (layer_state_is(0) && (abs(mouse_report.x) > 10 || abs(mouse_report.y) > 10)) {
+  if (layer_state_is(0) && (abs(mouse_report.x) > 5 || abs(mouse_report.y) > 5)) {
       layer_move(2);
   }
 
