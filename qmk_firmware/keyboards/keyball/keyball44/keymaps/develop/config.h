304caa0045b77fec7b14a53d6351a8c2ce8f99c3
kosaka_izumi <96470555+aki2ba@users.noreply.github.com>
2025-07-05 22:05:15 +0900

    #define TAPPING_TERM 200

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/develop/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/develop/config.h
@@ -19,6 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#define TAPPING_TERM 200
+
 #ifdef RGBLIGHT_ENABLE
 #    define RGBLIGHT_EFFECT_BREATHING
 #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
