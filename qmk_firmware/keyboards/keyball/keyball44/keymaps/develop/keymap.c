7fdf34415592a4f8b125b57838acfb32aac0c254
kosaka_izumi <96470555+aki2ba@users.noreply.github.com>
2025-07-05 23:22:37 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/develop/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/develop/keymap.c
@@ -34,26 +34,6 @@ enum custom_keycodes {
   TO0_HENK
 };
 
-enum {
-  TD_CTL_C_L5 = 0
-};
-
-void td_finished(tap_dance_state_t *state, void *user_data) {
-    if (state->count == 1 && !state->interrupted && !state->pressed) {
-        tap_code16(C(KC_C));  // タップ: Ctrl+C
-    } else {
-        layer_on(5);          // ホールド: レイヤー5
-    }
-}
-
-void td_reset(tap_dance_state_t *state, void *user_data) {
-    layer_off(5);
-}
-
-tap_dance_action_t tap_dance_actions[] = {
-    [TD_CTL_C_L5] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_finished, td_reset)
-};
-
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
@@ -87,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [4] = LAYOUT_universal(
     KC_TAB    , KC_Q    , KC_W           , LT(4,KC_NO)      , KC_R           , KC_T     ,                                        C(KC_Y)     , C(KC_PGUP)     , MS_BTN3        , C(KC_PGDN)      , C(KC_P)    , KC_BSPC   ,
     TO0_MHEN  , KC_A    , MS_BTN1        , CTL_T(MS_BTN3)   , ALT_T(MS_BTN2) , KC_G     ,                                        C(KC_H)     , MS_BTN1        , CTL_T(KC_UP)   , ALT_T(MS_BTN2)  , _______    , TO(3)     ,
-    TO0_HENK  , C(KC_Z) , C(KC_Y)        , TD(TD_CTL_C_L5)  , LT(5,KC_NO)    , KC_B     ,                                        A(KC_RGHT)  , LT(1,KC_LEFT)  , LT(3,KC_DOWN)  , SFT_T(KC_RGHT)  , KC_SLSH    , KC_DEL    ,
+    TO0_HENK  , C(KC_Z) , C(KC_Y)        , LT(6,KC_NO)      , LT(5,KC_NO)    , KC_B     ,                                        A(KC_RGHT)  , LT(1,KC_LEFT)  , LT(3,KC_DOWN)  , SFT_T(KC_RGHT)  , KC_SLSH    , KC_DEL    ,
                 _______    , _______       , _______        , LT(5,KC_SPC)      , KC_ENT,                KC_ESC   , KC_LWIN     , S(KC_CAPS)           , KC_HOME        , KC_END
   ),
   [5] = LAYOUT_universal(
@@ -215,6 +195,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             } else if (!record->event.pressed) {
                 unregister_code16(KC_LSFT);
+            }
+            return false;
+        case LT(6,KC_NO):
+            if (record->event.pressed) {
+                if (record->tap.count) {
+                    // タップ時に Ctrl+C を送信
+                    tap_code16(C(KC_C));
+                    return false; // 他の処理をスキップ
+                } else {
+                    // ホールド時にレイヤー3へ
+                    layer_on(5);
+                    return false;
+                }
+            } else if (!record->event.pressed) {
+                // ホールド解除でレイヤー3をオフ
+                layer_off(5);
             }
             return false;
     }
