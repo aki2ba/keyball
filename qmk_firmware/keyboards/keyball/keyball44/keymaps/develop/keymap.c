ba2a52b4f458e40ad0db8cb5e9db378b4b0a4a90
kosaka_izumi <96470555+aki2ba@users.noreply.github.com>
2025-07-05 23:11:30 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/develop/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/develop/keymap.c
@@ -85,15 +85,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  _______   , _______  , _______    ,        _______  , KC_PSCR  ,                   SS_LHOST  , SS_DC     , _______       , _______  , _______
   ),
   [4] = LAYOUT_universal(
-    KC_TAB    , KC_Q    , KC_W           , LT(4,KC_NO)    , KC_R           , KC_T     ,                                        C(KC_Y)     , C(KC_PGUP)     , MS_BTN3        , C(KC_PGDN)      , C(KC_P)    , KC_BSPC   ,
-    TO0_MHEN  , KC_A    , MS_BTN1        , CTL_T(MS_BTN3) , ALT_T(MS_BTN2) , KC_G     ,                                        C(KC_H)     , MS_BTN1        , CTL_T(KC_UP)   , ALT_T(MS_BTN2)  , _______    , TO(3)     ,
-    TO0_HENK  , C(KC_Z) , C(KC_Y)        , TD(TD_CTL_C_L5)    , LT(5,KC_NO)        , KC_B     ,                                        A(KC_RGHT)  , LT(1,KC_LEFT)  , LT(3,KC_DOWN)  , SFT_T(KC_RGHT)  , KC_SLSH    , KC_DEL    ,
+    KC_TAB    , KC_Q    , KC_W           , LT(4,KC_NO)      , KC_R           , KC_T     ,                                        C(KC_Y)     , C(KC_PGUP)     , MS_BTN3        , C(KC_PGDN)      , C(KC_P)    , KC_BSPC   ,
+    TO0_MHEN  , KC_A    , MS_BTN1        , CTL_T(MS_BTN3)   , ALT_T(MS_BTN2) , KC_G     ,                                        C(KC_H)     , MS_BTN1        , CTL_T(KC_UP)   , ALT_T(MS_BTN2)  , _______    , TO(3)     ,
+    TO0_HENK  , C(KC_Z) , C(KC_Y)        , TD(TD_CTL_C_L5)  , LT(5,KC_NO)    , KC_B     ,                                        A(KC_RGHT)  , LT(1,KC_LEFT)  , LT(3,KC_DOWN)  , SFT_T(KC_RGHT)  , KC_SLSH    , KC_DEL    ,
                 _______    , _______       , _______        , LT(5,KC_SPC)      , KC_ENT,                KC_ESC   , KC_LWIN     , S(KC_CAPS)           , KC_HOME        , KC_END
   ),
   [5] = LAYOUT_universal(
-    KC_DEL    , KC_P    , KC_O       , KC_I       , KC_U     , KC_Y  ,                                        C(KC_Y)     , C(KC_PGUP)     , MS_BTN3        , C(KC_PGDN)      , C(KC_P)    , KC_BSPC   ,
-    TO0_MHEN  , KC_S    , MS_BTN1    , S(KC_UP)   , MS_BTN2  , KC_H  ,                                        C(KC_H)     , MS_BTN1        , CTL_T(KC_UP)   , 	ALT_T(MS_BTN2)  , _______    , TO(3)     ,
-    TO0_HENK  , KC_Z    , S(KC_LEFT) , S(KC_DOWN) , S(KC_RGHT) , KC_N  ,                                        A(KC_RGHT)  , LT(1,KC_LEFT)  , LT(3,KC_DOWN)  , SFT_T(KC_RGHT)  , KC_SLSH    , KC_DEL    ,
+    KC_DEL    , KC_P    , KC_O     , KC_I     , KC_U  , KC_Y  ,                                        C(KC_Y)     , C(KC_PGUP)     , MS_BTN3        , C(KC_PGDN)      , C(KC_P)    , KC_BSPC   ,
+    TO0_MHEN  , KC_S    , MS_BTN1  , C(KC_D)  , KC_F  , KC_H  ,                                        C(KC_H)     , MS_BTN1        , CTL_T(KC_UP)   , 	ALT_T(MS_BTN2)  , _______    , TO(3)     ,
+    TO0_HENK  , KC_Z    , KC_X     , KC_C     , KC_V  , KC_N  ,                                        A(KC_RGHT)  , LT(1,KC_LEFT)  , LT(3,KC_DOWN)  , SFT_T(KC_RGHT)  , KC_SLSH    , KC_DEL    ,
                 _______  , _______       , _______        ,         KC_SPC      , KC_ENT,                KC_ESC   , KC_LWIN     , S(KC_CAPS)           , KC_HOME        , KC_END
   ),
 };
