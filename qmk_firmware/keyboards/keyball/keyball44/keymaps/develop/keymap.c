c910b5951432c808c3523f61dd5220b6cf33bf43
kosaka_izumi <96470555+aki2ba@users.noreply.github.com>
2025-07-13 15:19:24 +0900

    update layer5

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/develop/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/develop/keymap.c
@@ -71,9 +71,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 _______    , _______       , _______        , LT(5,KC_SPC)      , KC_ENT,                KC_ESC   , KC_LWIN     , S(KC_CAPS)           , KC_HOME        , KC_END
   ),
   [5] = LAYOUT_universal(
-    KC_DEL    , KC_P    , KC_O     , KC_I     , KC_U  , KC_Y  ,                                        C(KC_Y)     , C(KC_PGUP)     , MS_BTN3        , C(KC_PGDN)      , C(KC_P)    , KC_BSPC   ,
-    TO0_MHEN  , KC_S    , C(KC_S)  , C(KC_D)  , KC_F  , KC_H  ,                                        C(KC_H)     , MS_BTN1        , CTL_T(KC_UP)   , 	ALT_T(MS_BTN2)  , _______    , TO(3)     ,
-    TO0_HENK  , KC_Z    , KC_X     , KC_C     , KC_V  , KC_N  ,                                        A(KC_RGHT)  , LT(1,KC_LEFT)  , LT(3,KC_DOWN)  , SFT_T(KC_RGHT)  , KC_SLSH    , KC_DEL    ,
+    KC_DEL    , KC_P    , KC_O     , KC_I        , KC_U  , KC_Y  ,                                        C(KC_Y)     , C(KC_PGUP)     , MS_BTN3        , C(KC_PGDN)      , C(KC_P)    , KC_BSPC   ,
+    TO0_MHEN  , KC_S    , C(KC_S)  , LT(7,KC_NO) , ALT_T(KC_F)  , KC_H  ,                                        C(KC_H)     , MS_BTN1        , CTL_T(KC_UP)   , 	ALT_T(MS_BTN2)  , _______    , TO(3)     ,
+    TO0_HENK  , KC_Z    , KC_X     , KC_C        , SFT_T(KC_V)  , KC_N  ,                                        A(KC_RGHT)  , LT(1,KC_LEFT)  , LT(3,KC_DOWN)  , SFT_T(KC_RGHT)  , KC_SLSH    , KC_DEL    ,
                 _______  , _______       , _______        ,         KC_SPC      , KC_ENT,                KC_ESC   , KC_LWIN     , S(KC_CAPS)           , KC_HOME        , KC_END
   ),
 };
@@ -211,6 +211,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             } else if (!record->event.pressed) {
                 // ホールド解除でレイヤー3をオフ
                 layer_off(5);
+            }
+            return false;
+        case LT(7,KC_NO):
+            if (record->event.pressed) {
+                if (record->tap.count) {
+                    tap_code16(C(KC_D));
+                    return false;
+                } else {
+                    register_code16(KC_LCTL);
+                    return false;
+                }
+            } else if (!record->event.pressed) {
+                unregister_code16(KC_LCTL);
             }
             return false;
     }
