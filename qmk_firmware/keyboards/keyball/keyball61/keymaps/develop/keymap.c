97face7abdf28f2b5cf452d8748120e14d723ee3
kosaka.izumi <96470555+aki2ba@users.noreply.github.com>
2025-11-07 23:31:51 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/develop/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/develop/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB     , KC_Q     , WIN_T(KC_W)  , KC_E           , KC_R           , KC_T     ,                                        KC_Y     , KC_U        , KC_I           , KC_O            , KC_P     , KC_BSPC   ,
     KC_INT5    , KC_A     , KC_S         , CTL_T(KC_D)    , LT(2,KC_F)     , KC_G     ,                                        KC_H     , LT(2,KC_J)  , CTL_T(KC_K)    , ALT_T(KC_L)     , TO(2)    , TO(3)     ,
     KC_INT4    , KC_Z     , SFT_T(KC_X)  , LT(3,KC_C)     , LT(1,KC_V)     , KC_B     , _______  ,                _______  ,   KC_N     , LT(1,KC_M)  , LT(3,KC_COMM)  , 	SFT_T(KC_DOT) , KC_SLSH  , KC_DEL    ,
-    _______  ,  TO(4)   , _______  , _______  , S(KC_CAPS)   ,KC_SPC       , KC_ENT   ,                                   KC_ESC,   KC_LWIN,                                               _______  , _______  
+    _______  ,  TO(4)   , _______  , _______  , S(KC_CAPS)   ,KC_SPC       , KC_ENT   ,                                   KC_ESC,   KC_LWIN,                                               _______  , QK_BOOT  
   ),
 
   [1] = LAYOUT_right_ball(
