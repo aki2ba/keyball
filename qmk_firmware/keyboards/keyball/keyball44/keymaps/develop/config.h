dcfd82768cb7e36642579ba4d8077f5f0de6001e
kosaka.izumi <96470555+aki2ba@users.noreply.github.com>
2025-11-08 00:07:17 +0900

    Remove MASTER_RIGHT definition from config.h
    Removed the definition to fix the right side as master.

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/develop/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/develop/config.h
@@ -19,9 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-// 右側をマスターに固定
-#define MASTER_RIGHT
-
 #ifdef RGBLIGHT_ENABLE
 #    define RGBLIGHT_EFFECT_BREATHING
 #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
